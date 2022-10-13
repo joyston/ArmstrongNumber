dcc0074b99fc808ebd4c430f4a90007d6c862047
LAPTOP-A5M7S5QL\joyst <joystonf@gmail.com>
2022-10-14 01:40:07 +0530

    -Added function to check if Armstrong number alternatively without using string conversions

--- main.go
+++ main.go
@@ -9,18 +9,7 @@ func cube(x int) int {
 	return x * x * x
 }
 
-func main() {
-	var n, t int
-
-	fmt.Println("Enter the number of test cases:")
-	fmt.Scanln(&n)
-
-	var a []int
-	for i := 0; i < n; i++ {
-		fmt.Scanln(&t)
-		a = append(a, t)
-	}
-
+func IsArmstrongByString(a []int) {
 	var s string
 
 	for _, v := range a {
@@ -36,4 +25,41 @@ func main() {
 			fmt.Println("Not an Armstrong number")
 		}
 	}
+}
+
+func IsArmstrongWithoutString(a ...int) { //Variadic function
+	for _, v := range a {
+		sum := 0
+		t := v
+		for {
+			if t == 0 {
+				if v == sum {
+					fmt.Printf("%v is an Armstrong\n", v)
+				} else {
+					fmt.Printf("%v is not an Armstrong\n", v)
+				}
+				break
+			}
+			s := t % 10
+			sum += s * s * s
+			t /= 10
+		}
+	}
+}
+
+func main() {
+	var n, t int
+
+	fmt.Println("Enter the number of test cases:")
+	fmt.Scanln(&n)
+
+	var a []int
+	for i := 0; i < n; i++ {
+		fmt.Println("Enter the number to test:")
+		fmt.Scanln(&t)
+		a = append(a, t)
+	}
+
+	// IsArmstrongByString(a)
+	IsArmstrongWithoutString(a...)
 }
